bc273663208c700305e64e6b253d2eb516e19d44
ssom222 <30613069+somileeeeeeee@users.noreply.github.com>
2021-11-19 09:53:38 +0900

    Add files via upload

---
+++ BAEKJOON1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BAEKJOON1008 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+
+        System.out.println(A/B);
+        sc.close();
+    }
+}
